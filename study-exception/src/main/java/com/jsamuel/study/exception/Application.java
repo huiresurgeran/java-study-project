3a2641c73bd135ee050c156f31b1ad5d0b779def
huiresurgeran <313829429@qq.com>
2022-07-06 21:33:55 +0800

    feat(transfer): test long to int has exception or not

--- study-exception/src/main/java/com/jsamuel/study/exception/Application.java
+++ study-exception/src/main/java/com/jsamuel/study/exception/Application.java
@@ -2,4 +2,16 @@ package com.jsamuel.study.exception;
 
 public class Application {
 
+    public static void main(String[] args) {
+        try {
+            long l1 = 1234567890123456789L;
+            long l2 = 1L;
+            int i1 = (int) (l1 - l2);
+            System.out.print(i1);
+        } catch (ClassCastException e) {
+            e.printStackTrace();
+        }
+
+
+    }
 }
